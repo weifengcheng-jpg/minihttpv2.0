737d2c8b16323f2cdf80747269fc4455ecb92487
Weifc <2418060010@qq.com>
2020-09-21 00:22:38 +0800

    minihtpv2.0

---
+++ 12test/build/CMakeFiles/minihttp.exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minihttp.exe.dir/tests/minihttp.c.o"
+  "minihttp.exe.pdb"
+  "minihttp.exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/minihttp.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
